7fabf2751636d64ce02d7fb22a4f446e9017d6a7
Tri Pham <phamqtri1992@gmail.com>
2017-04-04 14:26:00 -0700

    Initial react set up with some test pages

---
+++ src/components/Zones.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Zone from './Zone';
+
+class Zones extends Component {
+    render() {
+        return(
+            <div>
+                <ol>
+                    <li>
+                        <Zone />
+                    </li>
+                    <li>
+                        <Zone />
+                    </li>
+                    <li>
+                        <Zone />
+                    </li>
+                </ol>
+            </div>
+        )
+    }
+};
+
+export default Zones;
